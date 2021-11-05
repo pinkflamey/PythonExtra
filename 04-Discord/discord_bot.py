48729bc0cc875c854dcb203bd4809a921c703ab9
pinkflamey <mavis@pinkflamey.nl>
2021-11-05 14:29:09 +0100

    push 1 04-discord

---
+++ 04-Discord/discord_bot.py
@@ -0,0 +1,15 @@
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    # De bot kan op veel verschillende servers draaien. Met deze regel code pak je de eerste server die deze bot heeft. Als jouw bot maar 1 server heeft is het makkelijk.
+    guild = client.guilds[0]
+    # De naam van de server printen we gelijk uit.
+    print(guild.name, "is the name of the server")
+
+    # We printen de naam van de bot user uit.
+    print(client.user, "has connected to the client")
+
+client.run("")

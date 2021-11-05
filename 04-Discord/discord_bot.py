c53d2944d61499026cc09332d3ec981350b2a968
pinkflamey <mavis@pinkflamey.nl>
2021-11-05 14:46:04 +0100

    finished

--- 04-Discord/discord_bot.py
+++ 04-Discord/discord_bot.py
@@ -1,4 +1,5 @@
 import discord
+import random
 
 client = discord.Client()
 
@@ -11,5 +12,30 @@ async def on_ready():
 
     # We printen de naam van de bot user uit.
     print(client.user, "has connected to the client")
+
+    channel = guild.text_channels[0]
+    print(channel.name, "is the name of the channel")
+    # await channel.send("I'm online!")
+
+@client.event
+async def on_message(message):
+
+    if message.author.bot == False:
+        if str(message.content).lower() == "je moeder":
+            m = random.randrange(1, 7)
+            if m == 1:
+                await message.channel.send("...is lelijk, @" + str(message.author))
+            elif m == 2:
+                await message.channel.send("...is een plopkoek, @" + str(message.author))
+            elif m == 3:
+                await message.channel.send("...is fucking lekker, @" + str(message.author))
+            elif m == 4:
+                await message.channel.send("...heb ik gisteravond genaaid, @" + str(message.author))
+            elif m == 5:
+                await message.channel.send("...is een 2/10, @" + str(message.author))
+            elif m == 6:
+                await message.channel.send("...is gewoon fucking triest, @" + str(message.author))
+
+
 
 client.run("")
